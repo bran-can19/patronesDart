f030dfa56c37ce3531e362c8ed5f2b66b06670e8
bran-can19 <Candanedabrandon@gmail.com>
2026-09-24 14:51:20 -0600

    Ejemplo con singlenton

--- singleton/configuracion.dart
+++ singleton/configuracion.dart
@@ -1,5 +1,12 @@
 class Configuracion {
-  String? idioma;
+  // es un constructor privado
+Configuracion._();
 
-  Configuracion(this.idioma);
+//lo que es static: vive durante todao el ciclo de la app
+//final: no se puede cambiar el valor de la variable  
+static final Configuracion  _istancia = Configuracion._();
+
+//es un constructor de tipo factory, que devuelve la misma instancia de la clase
+factory Configuracion() => _istancia;
+String idioma = 'es';
 }
--- singleton/main.dart
+++ singleton/main.dart
@@ -1,12 +1,19 @@
 import 'configuracion.dart';
 
 void main() {
-  final pantallaInicio = Configuracion('es');
-  final pantallaPerfil = Configuracion('en');
-
-  pantallaInicio.idioma = 'en';
+  final pantallaInicio = Configuracion();
+  final pantallaPerfil = Configuracion();
 
   print('idioma de la pantalla de inicio:${pantallaInicio.idioma}');
   print('idioma de la pantalla de inicio:${pantallaPerfil.idioma}');
+
+  pantallaInicio.idioma = 'en';
   
+  print('idioma de la pantalla de inicio:${pantallaInicio.idioma}');
+  print('idioma de la pantalla de inicio:${pantallaPerfil.idioma}');
+
+  print(identical(pantallaInicio, pantallaPerfil)
+      ?'ambas son iguales'
+      :'las pantallas son intancias diferentes.');
+
 }
